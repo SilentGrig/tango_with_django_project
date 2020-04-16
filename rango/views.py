cb1808ee787272122532ce6ada9b05f7fec23c3f
Gregory Thomas <SilentGrig@gmail.com>
2020-04-16 09:29:31 +0100

    chapter9 exercises, restrict access to add category and page, create restricted template

--- rango/views.py
+++ rango/views.py
@@ -39,6 +39,7 @@ def show_category(request, category_name_slug):
     return render(request, "rango/category.html", context=context_dict)
 
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -53,6 +54,7 @@ def add_category(request):
     return render(request, "rango/add_category.html", {"form": form})
 
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -158,4 +160,4 @@ def user_logout(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, "rango/restricted.html")
