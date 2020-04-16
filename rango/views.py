dfdd4b18c2198500b8a3540f15d18234512bb1c2
Gregory Thomas <SilentGrig@gmail.com>
2020-04-16 08:40:47 +0100

    add logout functionality

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
@@ -148,6 +148,12 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, "rango/login.html")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse("rango:index"))
 
 
 @login_required
