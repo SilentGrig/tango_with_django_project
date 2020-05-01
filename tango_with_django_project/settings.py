2d61450513d749f8a31020ca3caf83612c843a06
Gregory Thomas <SilentGrig@gmail.com>
2020-05-01 12:54:13 +0100

    update to remove secret key and prepare for pythonanywhere hosting

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -22,12 +22,14 @@ MEDIA_DIR = os.path.join(BASE_DIR, "media")
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ")fe6wupfkj=iqbd6kts4kd_v#0t7wjz^4f5pcjzodiptwlf-di"
+SECRET_KEY = None
+with open("secret.key", "r") as f:
+    SECRET_KEY = f.readline().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["rangogrig.pythonanywhere.com", "localhost"]
 
 
 # Application definition
